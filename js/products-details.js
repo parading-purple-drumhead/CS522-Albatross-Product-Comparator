1f475963f0af8c5cdc8b46f3a84f2a0da35ced80
Sudhanshu Basu Roy <sbasur2@uic.edu>
2022-10-08 10:11:45 -0500

    Updated lower priced color

--- js/products-details.js
+++ js/products-details.js
@@ -1,4 +1,6 @@
-const [[query1, product_type],[query2, product]] = new URLSearchParams(window.location.search)
+const [[query1, product_type], [query2, product]] = new URLSearchParams(
+  window.location.search,
+)
 
 console.log(product)
 console.log(product_type)
@@ -9,36 +11,39 @@ function getData() {
     .then((data) => {
       var products = data.data
 
-    
-      var device = products[product_type].filter((tech) => tech['url'] == product)
+      var device = products[product_type].filter(
+        (tech) => tech['url'] == product,
+      )
 
       console.log(device)
 
       const product_details = device[0]
 
       const ratings = product_details.ratings
-      const num_of_ratings = ratings['5'] + ratings['4'] + ratings['3'] + ratings['2'] + ratings['1']; 
+      const num_of_ratings =
+        ratings['5'] + ratings['4'] + ratings['3'] + ratings['2'] + ratings['1']
       const avg_rating =
-          (ratings['5'] * 5 +
+        (ratings['5'] * 5 +
           ratings['4'] * 4 +
           ratings['3'] * 3 +
           ratings['2'] * 2 +
-          ratings['1']) / num_of_ratings
+          ratings['1']) /
+        num_of_ratings
 
       console.log(avg_rating.toFixed(1))
 
       full_stars = ``
       half_stars = ``
       empty_stars = ``
-      
+
       for (i = 0; i < Math.floor(avg_rating); i++) {
-          full_stars += `<i class="bi bi-star-fill text-warning"></i>`
+        full_stars += `<i class="bi bi-star-fill text-warning"></i>`
       }
       for (i = 0; i < 5 - Math.ceil(avg_rating); i++) {
-          empty_stars += `<i class="bi bi-star text-warning"></i>`
+        empty_stars += `<i class="bi bi-star text-warning"></i>`
       }
       if (avg_rating % 1 > 0) {
-          half_stars = `<i class="bi bi-star-half text-warning"></i>`
+        half_stars = `<i class="bi bi-star-half text-warning"></i>`
       }
 
       console.log(product_details)
@@ -66,7 +71,9 @@ function getData() {
             ${full_stars + half_stars + empty_stars}            
             </span>
           </span>
-          <span style="font-size: 1.5rem;">${avg_rating.toFixed(1)}/5 (${num_of_ratings})</span>
+          <span style="font-size: 1.5rem;">${avg_rating.toFixed(
+            1,
+          )}/5 (${num_of_ratings})</span>
           <ul>
             <li id="logo" class="devices" style="padding-top:1em">
               <img
@@ -75,7 +82,9 @@ function getData() {
                 alt=""
                 class="logo"
               />
-              <h1 class="value" style="padding-left:0.5em">$${product_details['price'][0]['price']}</h1>
+              <h1 class="value ms-3" style="color: #41b02c">$${
+                product_details['price'][0]['price']
+              }</h1>
             </li>
             <li id="logo" class="devices"  style="padding-top:1em">
               <img
@@ -84,7 +93,9 @@ function getData() {
                 alt=""
                 class="logo"
               />
-              <h1 class="value" style="padding-left:0.5em">$${product_details['price'][1]['price']}</h1>
+              <h1 class="value ms-3">$${
+                product_details['price'][1]['price']
+              }</h1>
             </li>
           </ul>
           <ul class="text-secondary">
@@ -130,7 +141,9 @@ function getData() {
       </div>
       <div class="row mb-5">
         <div class="col-lg-6 offset-lg-3 text-center">
-          <a href="./product-compare.html?product_type=${product_type}&product=${product_details['url']}&compare=compapre" 
+          <a href="./product-compare.html?product_type=${product_type}&product=${
+        product_details['url']
+      }&compare=compapre" 
           class="btn btn-warning" style="font-size: 1.25rem;">
             <i class="bi bi-search me-2"></i>
             Compare with other products
@@ -166,7 +179,9 @@ function getData() {
         <div class="col-lg-6 px-5">
           <h3 class="text-center mb-3">Ratings and Reviews</h3>
           <div class="row">
-            <div class="col-lg-2 row align-content-center"><h1>${avg_rating.toFixed(1)}</h1></div>
+            <div class="col-lg-2 row align-content-center"><h1>${avg_rating.toFixed(
+              1,
+            )}</h1></div>
             <div class="col-lg-10 text-secondary" style="font-size: 1rem;">
               <div class="row mb-3">
                 <div class="col-lg-2">
